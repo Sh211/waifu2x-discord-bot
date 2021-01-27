6bb625bed374433d8c15f9e782fa8a0ae72220bc
Sh211 <68665004+Sh211@users.noreply.github.com>
2021-01-27 20:33:30 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 token = ""
-attachment_cache_path = ""
+cache_path = r""
+w2x_exec_path = r""
+cache_wipe_on_start = True
 logLevel = "INFO"  # Valid values are DEBUG, INFO, WARN, ERROR, CRITICAL. Defaults to warn.
