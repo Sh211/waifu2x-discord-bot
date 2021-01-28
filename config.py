3fe1ea77704bc505a5543ce36aa62810e158ab55
SH211 <68665004+Sh211@users.noreply.github.com>
2021-01-28 12:17:17 +0000

    Added a lot of exceptions and other additions.
    AMEND: Fixed an issue where the bot could only process .png images.
    Signed-off-by: SH211 <68665004+Sh211@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-token = ""  # Your discord bot access token
-cache_path = r""  # An empty folder for temporary storage of files
-w2x_exec_path = r""  # Path, including executable, to your version of waifu2x
-cache_wipe_on_start = True # Will wipe the cache_path on start if True. False is unsupported (for now)
-logLevel = "INFO"  # Valid values are DEBUG, INFO, WARN, ERROR, CRITICAL.
+token = ""
+cache_path = r""
+w2x_exec_path = r""
+cache_wipe_on_start = True
+logLevel = "INFO"  # Valid values are DEBUG, INFO, WARN, ERROR, CRITICAL. Defaults to warn.
